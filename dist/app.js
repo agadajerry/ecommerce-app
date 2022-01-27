205288a8fb82f4b3a2f0f382b276e141befd05e2
Idoko Agada Jerry <45728604+agadajerry@users.noreply.github.com>
2022-01-27 14:05:07 +0100

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
@@ -13,7 +12,6 @@ const product_routes_1 = __importDefault(require("./routes/product_routes"));
 const connectDb_1 = __importDefault(require("./config/connectDb"));
 const session = require("express-session");
 require("dotenv").config();
-const expressLayouts = require("express-ejs-layouts");
 const port = process.env.PORT || 4000;
 const app = (0, express_1.default)();
 const mongoDBStore = require("connect-mongodb-session")(session);
@@ -25,34 +23,17 @@ const store = new mongoDBStore({
     uri: uri,
     collection: "session"
 });
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    store: store,
-    cookie: { maxAge: 180 * 60 * 3 * 4000 }
-}));
-// Passport middleware
 app.use((0, cors_1.default)({
     origin: "https://ecommerce-api-server.herokuapp.com/product",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
 }));
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-app.set("views", path_1.default.join(__dirname, "../views"));
 app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
 //routes functions
 app.use("/product", product_routes_1.default);
-app.use((0, cors_1.default)({
-    origin: "https://ecommerce-api-server.herokuapp.com/product",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // allow session cookie from browser to pass through
-}));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next((0, http_errors_1.default)(404));
